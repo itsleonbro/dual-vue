95ecbb819c742afd2362df08554c8b9d90895f92
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-02-17 09:48:03 +0200

    test with new updates

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -2,14 +2,38 @@ import { DualVue } from "../../src";
 import { useState } from "react";
 
 function App() {
-  const [firstImage] = useState(`https://picsum.photos/id/237/800/400`);
-  const [secondImage] = useState(`https://picsum.photos/id/238/800/400`);
+  const [firstImage] = useState(
+    `https://images.unsplash.com/photo-1739538475083-43bbf5c47646?q=80&w=3136&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+  );
+  const [secondImage] = useState(
+    `https://images.unsplash.com/photo-1739582767192-3aa4d4811633?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+  );
 
   return (
     <>
-      <div style={{ width: "800px", height: "400px", margin: "40px auto" }}>
-        <DualVue firstImage={firstImage} secondImage={secondImage} />
-      </div>
+      {/* basic usage with defaults */}
+      <DualVue firstImage={firstImage} secondImage={secondImage} />
+      {/* custom dimensions */}
+
+      <DualVue
+        firstImage={firstImage}
+        secondImage={secondImage}
+        width="1000px"
+        height="300px"
+      />
+
+      {/* with additional styles */}
+      <DualVue
+        firstImage={firstImage}
+        secondImage={secondImage}
+        style={{ margin: "40px auto", border: "1px solid #ccc" }}
+      />
+      {/* with custom className */}
+      <DualVue
+        firstImage={firstImage}
+        secondImage={secondImage}
+        className="my-custom-class"
+      />
     </>
   );
 }
